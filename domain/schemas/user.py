672e3bba03aac5629d669c8d3d3c0ed927f4aec1
bond <amiram19@yandex.ru>
2025-05-04 20:10:11 +0300

    редактировал комменты

--- domain/schemas/user.py
+++ domain/schemas/user.py
@@ -4,19 +4,19 @@ from datetime import datetime
 
 
 class UserCreate(BaseModel):
-    """Схема для создания пользователя (входные данные от клиента)"""
+    """Создание пользователя (входные данные от клиента)"""
     email: EmailStr  # проверяет, что это валидная почта
-    name: str = Field(..., min_length=1, max_length=100)  # имя пользователя
+    name: str = Field(..., min_length=1, max_length=300, title="Имя пользователя")  # имя пользователя
 
 
 class UserUpdate(BaseModel):
-    """Схема для обновления пользователя"""
+    """Обновление пользователя"""
     email: EmailStr | None = None  # можно не обновлять
     name: str | None = None
 
 
 class UserRead(BaseModel):
-    """Схема для ответа клиенту (чтение пользователя)"""
+    """Ответ клиенту (чтение пользователя)"""
     id: UUID
     email: EmailStr
     name: str
