0bbd4e05faa5275a356fdc30e2cb4b864998f3d9
B0ond <amiram19@yandex.ru>
2025-05-21 22:24:41 +0300

    update 3

---
+++ infrastructure/db/models/base.py
@@ -0,0 +1,5 @@
+from sqlalchemy.orm import DeclarativeBase
+
+class BaseOrm(DeclarativeBase):
+    """Базовый класс для всех моделей БД."""
+    pass
--- infrastructure/db/models/user.py
+++ infrastructure/db/models/user.py
@@ -1,8 +1,1 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, DateTime, Column
-from sqlalchemy.orm import DeclarativeBase
-from uuid import UUID, uuid4
-from datetime import datetime, timezone
-
-class User(DeclarativeBase):
-    __tablename__ = 'user'
+from uuid import UUID
